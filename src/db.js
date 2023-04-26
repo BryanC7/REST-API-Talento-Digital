9e7789de7828d27c3848ca94fc9e2776d04d4982
Bryan <bryancabreram3@gmail.com>
2023-04-26 12:44:56 -0400

    Agregando ruta para sincronizar tablas en la nube

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
 import {Sequelize} from 'sequelize'
-import { DB_DATABASE, DB_HOST, DB_USER, DB_PASSWORD } from './config.js'
+import { DB_DATABASE, DB_HOST, DB_USER, DB_PASSWORD, DB_PORT } from './config.js'
 
 export const sequelize = new Sequelize (
     DB_DATABASE,
@@ -7,6 +7,7 @@ export const sequelize = new Sequelize (
     DB_PASSWORD,
     { 
         host: DB_HOST, 
+        DB_PORT, // Esta linea se comenta para utilizar la api en localhost
         dialect:'postgres',
         define: {
             timestamps: false
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,7 +1,17 @@
 import { Router } from "express"
+import { syncTables } from "../index.js"
 import { newUser, getTableUser, updateInfoUser, deleteUser, adminUser, getUsersCounts, getClients } from '../controllers/user.js'
 
 const router = Router()
+
+router.get('/sincronizar', (req, res) => {
+    try {
+        syncTables()
+        res.json({message: 'Tablas sincronizadas correctamente'})
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
 
 router.get('/users', getTableUser)
 router.get('/users/clients', getClients)
